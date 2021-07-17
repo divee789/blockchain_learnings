1a9f15936f2ad8701acbbb72353f916838c48f6e
Divine Olokor <divee789@gmail.com>
2021-07-17 22:38:01 +0100

    feat: finished campaign project, working on tests

--- Campaign/contracts/Campaign.sol
+++ Campaign/contracts/Campaign.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.6;
 
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(bytes32 _campaignName) public {
+        Campaign campaign = new Campaign(_campaignName, msg.sender);
+        deployedCampaigns.push(campaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
+}
+
 contract Campaign {
     struct Request {
         string description;
@@ -11,25 +24,29 @@ contract Campaign {
         mapping(address => bool) approvals;
     }
 
-    Request[] public requests;
     address public manager;
     uint256 public minimumContribution;
     mapping(address => bool) public approvers;
+    uint256 public approversCount;
+
+    uint256 numRequest;
+    mapping(uint256 => Request) requests;
+
+    constructor(uint256 minimum address creator) public {
+        manager = creator;
+        minimumContribution = minimum;
+    }
 
     modifier managerOnly() {
         require(msg.sender == manager);
         _;
     }
 
-    function startCampaign(uint256 minimum) public {
-        manager = msg.sender;
-        minimumContribution = minimum;
-    }
-
     function contribute() public payable {
         require(msg.value > minimumContribution);
 
         approvers[msg.sender] = true;
+        approversCount++;
     }
 
     function createRequest(
@@ -38,13 +55,34 @@ contract Campaign {
         address recipient
     ) public managerOnly {
         require(approvers[msg.sender]); // You must contribute before you can create a request
-        Request memory newRequest = Request(
-            description,
-            value,
-            recipient,
-            false
-        );
 
-        requests.push(newRequest);
+        Request storage r = requests[numRequest++];
+
+        r.approvalCount = 0;
+        r.value = value;
+        r.description = description;
+        r.recipient = recipient;
+        r.complete = false;
+    }
+
+    function approveRequest(uint256 index) public {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]); // You must be an approver to approve a request
+        require(!request.approvals[msg.sender]); // You can't approve a request twice
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint256 index) public managerOnly {
+        Request storage request = requests[index];
+
+        require(request.approvalCount >= approversCount); // Request approval count must be greater than or equal to the minimum approvalCount specified;
+        require(!request.complete); // Request must not be complete
+
+        request.recipient.transfer(request.value);
+
+        request.complete = true; // Mark request as complete
     }
 }
